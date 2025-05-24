145ba6001a20aaaab45018b458e9c210b0aa130a
eleadn <eleano.dn@gmail.com>
2025-05-24 22:42:47 +0200

    addDropdown

---
+++ index.js
@@ -0,0 +1,37 @@
+exports.createDropdown = function (dropdownMenu) {
+	const content = dropdownMenu.querySelectorAll("*");
+
+	const show = function () {
+		for (const elem in content) {
+			elem.style.visibility = "visible";
+		}
+	};
+
+	const hide = function () {
+		for (const elem in content) {
+			elem.style.visibility = "collapse";
+		}
+	};
+
+	const onClickOutside = function (event) {
+		if (
+			!dropdownMenu.contains(event.target) &&
+			!content.some((value) => {
+				value.contains(event.target);
+			})
+		) {
+			this.hide();
+			document.removeEventListener("keydown", onClickOutside);
+		}
+	};
+
+	dropdownMenu.addEventListener("click", () => {
+		this.show();
+	});
+
+	document.addEventListener("click", onClickOutside);
+
+	this.hide();
+
+	return { show, hide };
+};
